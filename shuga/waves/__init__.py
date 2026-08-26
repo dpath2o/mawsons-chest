a8086458995ae38dfacd26992ac4a5bcbd52639d
Daniel Patrick Atwater <dpath2o@users.noreply.github.com>
2026-08-27 07:52:34 +1000

    Export WHACS wave regridding API

--- shuga/waves/__init__.py
+++ shuga/waves/__init__.py
@@ -1,1 +1,10 @@
 from .cawcr import CAWCRRegridConfig, CAWCRRegridder
+from .whacs import WHACSRegridder, WHACS_SOURCE_ROOT, cice25_frequency_metadata
+
+__all__ = [
+    "CAWCRRegridConfig",
+    "CAWCRRegridder",
+    "WHACSRegridder",
+    "WHACS_SOURCE_ROOT",
+    "cice25_frequency_metadata",
+]
